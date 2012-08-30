dd9516feaf7939e42560928ed700f305068c347a
Kazu <saori.cube@gmail.com>
2012-08-30 21:44:41 +0900

    taskD:Create carts

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,4 +9,19 @@ class Product < ActiveRecord::Base
     with: %r{\.(gif|jpg|png)$}i,
     message: 'only URL of GIF, JPG or PNG'
   }
+  
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+  
+  private
+    
+    # ensure not referenced by any line itme
+    def ensure_not_referenced_by_any_line_item
+      if line_items.empty?
+        return true
+      else
+        errors.add(:base, 'exist line item')
+        return false
+      end
+    end
 end
