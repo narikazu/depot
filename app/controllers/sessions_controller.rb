d7586a29f4edf7fdd7284fac32b4114805f18522
Kazu <saori.cube@gmail.com>
2012-11-14 20:44:13 +0900

    generate controller Sessions and Admin

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+# encoding: utf-8
+
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by_name(params[:name])
+    if user and user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to admin_url
+    else
+      redirect_to login_url, alert: "無効なユーザ／ パスワードの組み合わせです"
+    end
+  end
+
+  def destroy
+  end
+end
